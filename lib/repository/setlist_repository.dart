934f59e69606c2def2b73220f2e65c3652ca5871
Tim Kelly <timkellypa@gmail.com>
2023-10-09 22:30:50 -0400

    Fix setlist deletion issue. - Delete all tracks first to fix foreign key failure.

--- lib/repository/setlist_repository.dart
+++ lib/repository/setlist_repository.dart
@@ -1,20 +1,22 @@
 import 'package:in_the_pocket/model/setlistdb.dart';
 import 'package:in_the_pocket/model/table_base_override.dart';
 import 'package:in_the_pocket/repository/repository_base.dart';
+import 'package:in_the_pocket/repository/track_repository.dart';
 
 class SetListRepository extends RepositoryBase<Setlist> {
   @override
-  Future<List<Setlist>> fetch({
-    bool Function(Setlist)? filter, 
-    String? whereClause,
-    String? whereParameter
-  }) async {
+  Future<List<Setlist>> fetch(
+      {bool Function(Setlist)? filter,
+      String? whereClause,
+      String? whereParameter}) async {
     SetlistFilterBuilder setListQuery = Setlist().select();
     if (whereClause != null) {
-      setListQuery = setListQuery.where(whereClause, parameterValue: whereParameter);
+      setListQuery =
+          setListQuery.where(whereClause, parameterValue: whereParameter);
     }
 
-    List<Setlist> setLists = await setListQuery.orderBy(TableBase.SORT_ORDER_COLUMN).toList();
+    List<Setlist> setLists =
+        await setListQuery.orderBy(TableBase.SORT_ORDER_COLUMN).toList();
 
     if (filter != null) {
       setLists = setLists.where(filter).toList();
@@ -40,6 +42,14 @@ class SetListRepository extends RepositoryBase<Setlist> {
   @override
   Future<void> delete(String id) async {
     final Setlist? setlist = await Setlist().getById(id);
+    final List<SetlistTrack>? setlistTracks =
+        await setlist?.getSetlistTracks()?.toList(preload: true);
+
+    if (setlistTracks != null) {
+      for (SetlistTrack setlistTrack in setlistTracks) {
+        await TrackRepository().delete(setlistTrack.id!);
+      }
+    }
     await setlist?.delete();
   }
 }
